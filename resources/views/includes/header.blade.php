ae5d019707de5b850bf73084bad4724732433402
minibrary <minibrary@gmail.com>
2017-01-16 23:48:04 +0900

    gravata

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -214,16 +214,16 @@
         <li class="dropdown user user-menu">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
             <img src="{{ Gravatar::src(Auth::user()->email) }}" alt="Avatar of {{ Auth::user()->name }}" class="user-image">
-            <span class="hidden-xs">Alexander Pierce</span>
+            <span class="hidden-xs">{{ Auth::user()->name }}</span>
           </a>
           <ul class="dropdown-menu">
             <!-- User image -->
             <li class="user-header">
-              <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+              <img src="{{ Gravatar::src(Auth::user()->email) }}" alt="Avatar of {{ Auth::user()->name }}" class="img-circle">
 
               <p>
-                Alexander Pierce - Web Developer
-                <small>Member since Nov. 2012</small>
+                {{ Auth::user()->name }}
+                <small>Member since {{ Auth::user()->created_at->format('d M Y') }}</small>
               </p>
             </li>
             <!-- Menu Body -->
