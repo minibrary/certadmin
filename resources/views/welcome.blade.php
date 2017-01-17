083912adeb66964f387a25e345530ec71dbb9db8
Minibrary Admin <pi@minibrary.com>
2017-01-18 07:37:35 +0900

    small change front

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Cert Admin</title>
+        <title> Certivel </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,12 +69,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Cert Admin
+                    Certivel
                 </div>
 
                 <div class="links">
                     <a href="{{ url('/login') }}">Login</a>
                     <a href="{{ url('/register') }}">Register</a>
+                    <a href="{{ url('/password/reset') }}">Reset Password</a>
                 </div>
             </div>
         </div>
