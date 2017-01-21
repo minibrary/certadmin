e6c693e3e2e02b5f5b6e0adda30019a82a24a57b
minibrary <minibrary@gmail.com>
2017-01-21 20:13:31 +0900

    Trait use?

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Trait\CaptchaTrait;
 
 class RegisterController extends Controller
 {
