8573cead6860732a0608261913c83f425ca1227d
minibrary <minibrary@gmail.com>
2017-01-23 21:41:41 +0000

    call Auth middleware

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -14,6 +14,11 @@ class CertificateController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
       $user = \Auth::user();    // 1
