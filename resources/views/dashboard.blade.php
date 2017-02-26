9ac6f5e8bef842314d55b2d715526f218b8f2687
minibrary <minibrary@gmail.com>
2017-02-26 17:00:33 +0900

    contents add

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,9 +26,6 @@
       <div class="box box-solid box-success box-message">
         <div class="box-header with-border">
           <h3 class="box-title">Information</h3>
-          <div class="box-tools pull-right">
-            <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-          </div><!-- /.box-tools -->
         </div><!-- /.box-header -->
         <div class="box-body">
           {{Session::get('message')}}
@@ -36,8 +33,36 @@
       </div><!-- /.box -->
       @endif
 
-      <P>Hello, world!</p>
-
+      <P><h4>Good day, {{ Auth::user()->name }}!</h4></p>
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="info-box">
+              <a href="{{ url('/list') }}"><span class="info-box-icon bg-aqua"><i class="fa fa-lock"></i></span></a>
+              <div class="info-box-content">
+                <span class="info-box-text">Total</span>
+                <span class="info-box-number">{{ App\Certificate::where('user_id', Auth::user()->id)->count() }}</span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </div><!-- /.col -->
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="info-box">
+              <a href="{{ url('/list/warning') }}"><span class="info-box-icon bg-yellow"><i class="fa fa-lock"></i></span></a>
+              <div class="info-box-content">
+                <span class="info-box-text">Warning</span>
+                <span class="info-box-number">{{ App\Certificate::where('user_id', Auth::user()->id)->whereBetween('daysleft', [30, 90])->count() }}</span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </div><!-- /.col -->
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="info-box">
+              <a href="{{ url('/list/danger') }}"><span class="info-box-icon bg-red"><i class="fa fa-lock"></i></span></a>
+              <div class="info-box-content">
+                <span class="info-box-text">Danger</span>
+                <span class="info-box-number">{{ App\Certificate::where('user_id', Auth::user()->id)->where('daysleft', '<', '30')->count() }}</span>
+              </div><!-- /.info-box-content -->
+            </div><!-- /.info-box -->
+          </div><!-- /.col -->
+        </div><!-- /.row -->
     </div>
   </div>
 </section>
