546a3b7e10b813358fac52619ebad99e897c2785
minibrary <minibrary@gmail.com>
2017-02-27 22:46:02 +0900

    search box remove

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -76,14 +76,7 @@
             <div class="box">
               <div class="box-header">
                 <h3 class="box-title">Top 5 Certificate by neareast expiry date</h3>
-
-                <div class="box-tools">
-                  <div class="input-group input-group-sm" style="width: 150px;">
-                    <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
-                  </div>
-                </div>
               </div>
-              <!-- /.box-header -->
               <div class="box-body table-responsive no-padding">
                 <table class="table table-hover">
                   <tr>
