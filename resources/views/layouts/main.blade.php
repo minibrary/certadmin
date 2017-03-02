966f9ec8be1e28dffa24cfc3b4f980c8d97567bb
minibrary <minibrary@gmail.com>
2017-03-02 23:15:46 +0900

    adsense test

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -188,18 +188,18 @@
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Certivel-2 -->
-    <ins class="adsbygoogle"
-         style="display:block"
-         data-ad-client="ca-pub-2882078161937378"
-         data-ad-slot="5930523049"
-         data-ad-format="auto"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
 
   @yield('content')
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Certivel-2 -->
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-2882078161937378"
+       data-ad-slot="5930523049"
+       data-ad-format="auto"></ins>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
 
   </div>
   <!-- /.content-wrapper -->
