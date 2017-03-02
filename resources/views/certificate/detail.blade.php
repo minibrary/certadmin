9ff1f33df6b3c30feab73774afc1d06d1472b64e
minibrary <minibrary@gmail.com>
2017-03-02 22:12:13 +0900

    adsense test

--- resources/views/certificate/detail.blade.php
+++ resources/views/certificate/detail.blade.php
@@ -22,8 +22,6 @@
 @endpush
 
 @section('content')
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
   <h1>
--- resources/views/certificate/list.blade.php
+++ resources/views/certificate/list.blade.php
@@ -11,8 +11,6 @@
 @endpush
 
 @section('content')
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
   <h1>
